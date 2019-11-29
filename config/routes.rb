3b8e8013dac7915a2f26c6d97f3fe19947c33357
Nox Yu <nox19990209@gmail.com>
2019-11-28 23:58:35 -0500

    project controller and view CRUD setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'project/new'
+  get 'project/create'
+  get 'project/update'
+  get 'project/edit'
+  get 'project/delete'
+  get 'project/index'
+  get 'project/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
