404f848eae275bd9dc3a37b0780d585f7f793bb7
Nox Yu <nox19990209@gmail.com>
2019-11-29 15:55:36 -0500

    created the right projectsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'project/new'
-  get 'project/create'
-  get 'project/update'
-  get 'project/edit'
-  get 'project/delete'
-  get 'project/index'
-  get 'project/show'
+  resources :projects
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
